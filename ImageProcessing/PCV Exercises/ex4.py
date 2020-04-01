9b2b07e426d92e754a48fceffd4077bd642e118c
harishankar-18 <61908805+harishankar-18@users.noreply.github.com>
2020-04-01 12:14:22 +0530

    Update ex4.py

--- ImageProcessing/PCV Exercises/ex4.py
+++ ImageProcessing/PCV Exercises/ex4.py
@@ -5,7 +5,14 @@ from skimage import filters
 
 im = array(Image.open("dog.jpg").convert('L'))
 
-edges = filters.sobel(im)
-plt.imshow(edges)
+imx = pl.zeros(im.shape)
+filters.sobel(im,1,imx)
+
+imy = pl.zeros(im.shape)
+filters.sobel(im,0,imy)
+
+magnitude = pl.sqrt(imx**2+imy**2)
+
+plt.imshow(magnitude)
 plt.title('sobel', size=20)
 plt.show()
